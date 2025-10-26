c2310b4ddc693465a7016b36b5e9a7089ddfa7f6
Umar Alfarouk <medrivia@gmail.com>
2025-10-26 17:10:12 +0700

    Update nu, hypr + nvim

---
+++ nvim/ulazy/lspconfig.lua
@@ -0,0 +1,5 @@
+-- id=p-lspconfig.lua
+-- https://github.com/neovim/nvim-lspconfig
+return {
+	"neovim/nvim-lspconfig",
+}
---
+++ nvim/ulazy/markview.lua
@@ -0,0 +1,11 @@
+return {
+	enabled = false,
+    "OXY2DEV/markview.nvim",
+    lazy = false,
+
+    -- For blink.cmp's completion
+    -- source
+    -- dependencies = {
+    --     "saghen/blink.cmp"
+    -- },
+};
